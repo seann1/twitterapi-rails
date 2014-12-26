4944f29411feb8eb77187f977a88178893b55aa4
Sean <seanniesen@gmail.com>
2014-12-25 22:15:13 -0800

    seperate hashtags from text on tweets

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,5 +8,26 @@ class PostsController < ApplicationController
 		  config.oauth_token = ENV['ACCESS_TOKEN']
 		  config.oauth_token_secret = ENV['ACCESS_SECRET']
 		end
+
+		@tweet = @client.search("#cocacola", result_type: "recent", lang: "en").first
+		tweet = @tweet.text.split(' ')
+		no_link_array = []
+		hashtags = []
+		
+		tweet.each do |word|
+			if word[0..3].downcase == "http"
+				#dont push to new array
+			elsif word[0] == "#"
+				#push hashtags to seperate array
+				hashtags << word
+			else
+				#push word to new array
+				no_link_array << word
+			end
+		end
+
+		@cleantweet = no_link_array.join(" ")
+		@hashtag_string = hashtags.join(" ")
+
 	end
 end
